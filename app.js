43aa1478a4ea8e9963c6475bcd573d2282c83969
S0Bolb2 <yar.ponomarev.2009@gmail.com>
2024-08-02 22:22:31 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -6,63 +6,43 @@ tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
 let item = "";
-let amounts = {
-  "1": 0, // Bitcoin
-  "2": 0, // Ethereum
-  "3": 0 // Tether
-};
+let clickCount = 0; // Добавляем переменную для подсчета кликов
 
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
 
-// Добавляем поля для ввода количества
-let inputAmount1 = document.createElement("input");
-inputAmount1.setAttribute("type", "number");
-inputAmount1.setAttribute("placeholder", "Введите количество Bitcoin");
-document.body.appendChild(inputAmount1);
-
-let inputAmount2 = document.createElement("input");
-inputAmount2.setAttribute("type", "number");
-inputAmount2.setAttribute("placeholder", "Введите количество Ethereum");
-document.body.appendChild(inputAmount2);
-
-let inputAmount3 = document.createElement("input");
-inputAmount3.setAttribute("type", "number");
-inputAmount3.setAttribute("placeholder", "Введите количество Tether");
-document.body.appendChild(inputAmount3);
-
 btn1.addEventListener("click", function(){
+  clickCount++; // Увеличиваем счетчик при каждом клике
   if (tg.MainButton.isVisible) {
     tg.MainButton.hide();
   }
   else {
-    amounts["1"] = parseInt(inputAmount1.value) || 0; // Получаем количество для Bitcoin
-    tg.MainButton.setText(Вы выбрали Bitcoin! (${amounts["1"]}));
+    tg.MainButton.setText(Вы выбрали Bitcoin! (Клик: ${clickCount})); // Выводим количество кликов в тексте кнопки
     item = "1";
     tg.MainButton.show();
   }
 });
 
 btn2.addEventListener("click", function(){
+  clickCount++;
   if (tg.MainButton.isVisible) {
     tg.MainButton.hide();
   }
   else {
-    amounts["2"] = parseInt(inputAmount2.value) || 0; // Получаем количество для Ethereum
-    tg.MainButton.setText(Вы выбрали Ethereum! (${amounts["2"]}));
+    tg.MainButton.setText(Вы выбрали Ethereum! (Клик: ${clickCount}));
     item = "2";
     tg.MainButton.show();
   }
 });
 
 btn3.addEventListener("click", function(){
+  clickCount++;
   if (tg.MainButton.isVisible) {
     tg.MainButton.hide();
   }
   else {
-    amounts["3"] = parseInt(inputAmount3.value) || 0; // Получаем количество для Tether
-    tg.MainButton.setText(Вы выбрали Tether! (${amounts["3"]}));
+    tg.MainButton.setText(Вы выбрали Tether! (Клик: ${clickCount}));
     item = "3";
     tg.MainButton.show();
   }
@@ -70,7 +50,7 @@ btn3.addEventListener("click", function(){
 
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
-  tg.sendData(item + "," + amounts[item]); // Отправляем данные в виде строки "item,amount"
+  tg.sendData(item);
 });
 
 
